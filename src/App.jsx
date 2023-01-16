5794e208b78d284be9bbe8f26d18b384290be80f
Benson Njuguna <njugunab655@gmail.com>
2023-01-16 09:30:14 +0300

    Add listings to display

--- src/App.jsx
+++ src/App.jsx
@@ -8,12 +8,23 @@ export default function App() {
     setItems(prev => [...prev, item])
   }
 
+  const elements = items.map(item => {
+    return (
+      <div className="listing">
+        <p>{item}</p>
+        <span className="delete">X</span>
+      </div>
+    )
+  })
 
   return (
     <div className="container">
       <h1>Items To Buy</h1>
       <div className="items-to-buy">
         <Form setBuy={setBuy} />
+      </div>
+      <div className="listings">
+        {elements}
       </div>
     </div>
   )
