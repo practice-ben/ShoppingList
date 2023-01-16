0b5ad00ad7cca0528121b3506ea3cd8bc0d75492
Benson Njuguna <njugunab655@gmail.com>
2023-01-16 12:39:52 +0300

    Added onkeyup event to form

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -3,13 +3,21 @@ import React from "react";
 export default function Form({setBuy}) {
     const [name, setName] = React.useState("")
     return (
-        <form>
+        <form onSubmit={(e) => {
+            e.preventDefault()
+        }}>
             <input 
                 required
                 type="text"
                 placeholder="Add a new item"
                 value={name}
                 onChange={(e) => setName(e.target.value)}
+                onKeyUp={(e) => {
+                    if(e.key === "Enter" && name) {
+                        setBuy(name);
+                        setName("")
+                    }
+                } }
             />
             <button type="button" onClick={() => {
                 if(name){
